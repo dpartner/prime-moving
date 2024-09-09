d6fb132e2f2a56185b8567ce5a1ae8e1760e549c
dpartner <d.partner.ua@gmail.com>
2024-09-09 23:20:24 +0300

    build

---
+++ vite.confog.js
@@ -0,0 +1,22 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import reactRefresh from "@vitejs/plugin-react-refresh";
+
+const root = resolve(__dirname, "./");
+const outDir = resolve(__dirname, "dist");
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  root,
+  plugins: [reactRefresh()],
+  build: {
+    outDir,
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(root, "index.html"),
+        policy: resolve(root, "pages", "policy.html"),
+      },
+    },
+  },
+});
